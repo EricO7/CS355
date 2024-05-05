691a16f5a0ce1d838990436bd9978bdece24cbb0
EricO7 <Eortega1@caldwell.edu>
2024-05-05 12:30:39 -0400

    hi

--- assignment 4/index.js
+++ assignment 4/index.js
@@ -3,12 +3,18 @@ const cors = require('cors')
   
 // enabling CORS for any unknown origin(https://xyz.example.com) 
 app.use(cors()); 
+app.use(express.static('public'));
+
+
+const randInt = n=>Math.floor(n*Math.random());  // returns random integer between 0 and n, not including n
+const getRandomItemFromArray = arr=>arr[randInt(arr.length)];  // returns a random item from an array
+
 const breeds = {
-    "husky":["../husky.jpg"],
-    "boxer":["../boxer.jpg"], 
-    "beagle":["../beagle.jpg"],
-    "corgi":["../corgi.jpg"],
-    "collie":["../collie.jpg"]
+    "husky":["imgs/husky.jpg"],
+    "boxer":["imgs/boxer.jpg", "imgs/beagle.jpg"], 
+    "beagle":["imgs/beagle.jpg"],
+    "corgi":["imsg/corgi.jpg"],
+    "collie":["imgs/collie.jpg"]
 }
 app.get('/breeds/',(req,res)=>{ // show all users
     res.send(breeds);
@@ -16,8 +22,9 @@ app.get('/breeds/',(req,res)=>{ // show all users
 
 app.get('/image/:breed',(req,res)=>{
     const b=req.params.breed; // show tweets for one user
-    console.log(breeds[b]);
-    res.send(breeds[b]);
+    let photo=getRandomItemFromArray(breeds[b]);
+    console.log(photo);
+    res.send({photo});
 });
 
 app.listen(3000);
